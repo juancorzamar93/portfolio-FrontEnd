5c51869af50cfabba96e1295aa24a77b3c61dbc3
juancz8 <juancorzamar@gmail.com>
2022-07-07 01:18:21 -0300

    primera version

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PortfolioService } from './servicios/portfolio.service';
+import {HttpClientModule} from '@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    PerfilComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
